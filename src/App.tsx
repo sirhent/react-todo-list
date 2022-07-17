768c7356365ee59a3b95e4cca3a1c5bc039ecb4d
Douglas Matias <douglas.mts7@hotmail.com>
2022-07-17 14:34:22 -0300

    Create EmptyMessage.tsx component

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import "./global.css";
 import styles from "./App.module.css";
 
 import todoLogo from "./assets/images/brand/Todo-Logo.svg";
+import { EmptyMessage } from "./components/EmptyMessage";
 
 export type TodoType = {
   id: string,
@@ -143,21 +144,7 @@ function App() {
           <LineDivider />
 
           {isTodoListEmpty ? (
-            <div className={styles["c-empty-tasklist"]}>
-              <ClipboardText 
-                className={styles["c-empty-tasklist__icon"]}
-                weight="thin"
-                size={64}
-              />
-              <div className={styles["c-empty-tasklist__content"]}>
-                <h2 className={styles["c-empty-tasklist__content__title"]}>
-                  Você ainda não tem tarefas cadastradas.
-                </h2>
-                <p className={styles["c-empty-tasklist__content__paragraph"]}>
-                  Crie tarefas e organize seus itens a fazer.
-                </p>
-              </div>
-            </div>
+            <EmptyMessage />
           ) : (
             <div className={styles["o-tasks-container"]}>
               {todos.map(todo => {
---
+++ src/components/EmptyMessage.tsx
@@ -0,0 +1,23 @@
+import { ClipboardText } from "phosphor-react";
+
+import styles from "./EmptyMessage.module.css";
+
+export function EmptyMessage() {
+  return (
+    <div className={styles["c-empty-tasklist"]}>
+      <ClipboardText
+        className={styles["c-empty-tasklist__icon"]}
+        weight="thin"
+        size={64}
+      />
+      <div className={styles["c-empty-tasklist__content"]}>
+        <h2 className={styles["c-empty-tasklist__content__title"]}>
+          Você ainda não tem tarefas cadastradas.
+        </h2>
+        <p className={styles["c-empty-tasklist__content__paragraph"]}>
+          Crie tarefas e organize seus itens a fazer.
+        </p>
+      </div>
+    </div>
+  );
+}
